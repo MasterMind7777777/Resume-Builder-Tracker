c1a84017621e7ee2e6dc97c6bceeac3f472a5a03
MasterMind7777777 <go.go.nest.nest@gmail.com>
2023-06-23 15:36:13 +0300

    created resume builder with existing blocks

--- resume_builder_tracker/resume/urls.py
+++ resume_builder_tracker/resume/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import resume_view
+from .views import resume_view, generate_resume
 
 app_name = 'resume'  # Replace 'resume' with the name of your app
 
 urlpatterns = [
     path('<int:resume_id>/', resume_view, name='resume_view'),
+    path('<int:resume_id>/generate/', generate_resume, name='generate_resume'),
     # Add more URL patterns if needed
 ]
--- resume_builder_tracker/resume/views.py
+++ resume_builder_tracker/resume/views.py
@@ -10,3 +10,21 @@ def resume_view(request, resume_id):
     }
 
     return render(request, 'resume/resume_template.html', context)
+
+def generate_resume(request, resume_id):
+    # Retrieve the necessary data from your models or any other source
+    resume = Resume.objects.get(id=resume_id)
+
+    # Define an array or list of available information sections
+    info_sections = [
+        {'include': True, 'template': 'resume/blocks/base/personal_info.html'},
+        {'include': True, 'template': 'resume/blocks/base/education.html'},
+        {'include': True, 'template': 'resume/blocks/base/experience.html'},
+        {'include': True, 'template': 'resume/blocks/base/skills.html'},
+    ]
+
+    # Render the HTML template with the context
+    return render(request, 'resume/layouts/base_layout.html', {
+        'resume': resume,
+        'info_sections': info_sections,
+    })
